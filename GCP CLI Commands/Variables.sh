c178af67e62c245f322dbef2229f9e2764dda255
Abhay Indriya <136146181+Abhay1613@users.noreply.github.com>
2024-12-28 13:28:56 +0530

    Create Variables.sh

---
+++ GCP CLI Commands/Variables.sh
@@ -0,0 +1,3 @@
+export REGION=REGION    # To create a variable for region
+export ZONE=Zone        # To create a variable for the zone
+export PROJECT_ID=Project ID    # Create an environment variable to store your Project ID
